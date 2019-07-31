1e476c2a6030007e7893a816e999bcc90efa03e5
jpromanonet <programming@jpromano.net>
2019-07-30 22:08:53 -0300

    Prettier and more args added

--- app.js
+++ app.js
@@ -1,23 +1,27 @@
 window.addEventListener("load", () => {
   let long;
   let lat;
+  let temperatureDescription = document.querySelector(
+    ".temperature-description"
+  );
+  let temperatureDegree = document.querySelector(".temperature-degree");
 
   if (navigator.geolocation) {
     navigator.geolocation.getCurrentPosition(position => {
-        long = position.coords.longitude;
-        lat = position.coords.latitude;
+      long = position.coords.longitude;
+      lat = position.coords.latitude;
 
-        const proxy = 'https://cors-anywhere.herokuapp.com/';
-        const api = `https://api.darksky.net/forecast/29ba721fbc0945c16918046de04f4228/${lat},${long}`;
+      const proxy = "https://cors-anywhere.herokuapp.com/";
+      const api = `${proxy}https://api.darksky.net/forecast/29ba721fbc0945c16918046de04f4228/${lat},${long}`;
 
-        fetch(api)
-            .then(response =>{
-                return response.json();
-            })
-            .then(data => {
-                console.log(data);
-            })
-
+      fetch(api)
+        .then(response => {
+          return response.json();
+        })
+        .then(data => {
+          console.log(data);
+          const { temperature, summary } = data.currently;
+        });
     });
   } else {
     h1.textContent =
