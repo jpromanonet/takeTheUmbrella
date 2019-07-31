519a040a3dc340af9f3015e4f5783fe70818e7e7
jpromanonet <programming@jpromano.net>
2019-07-30 21:53:34 -0300

    darksky api added

--- app.js
+++ app.js
@@ -3,11 +3,14 @@ window.addEventListener("load", () => {
   let lat;
 
   if (navigator.geolocation) {
-      navigator.geolocation.getCurrentPosition(position =>{
-        long = position.coords.longitude;
-        lat = position.coords.latitude;
-      });
-  }else{
-      h1.textContent = 'Please allow geolocation for the service to work properly, thank u!'
+    navigator.geolocation.getCurrentPosition(position => {
+      long = position.coords.longitude;
+      lat = position.coords.latitude;
+
+      const api = `https://api.darksky.net/forecast/29ba721fbc0945c16918046de04f4228/${lat},${long}`;
+    });
+  } else {
+    h1.textContent =
+      "Please allow geolocation for the service to work properly, thank u!";
   }
 });
